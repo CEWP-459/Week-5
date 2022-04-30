b69c7780bc332193a171e8cf29dd06cc6d6ee191
Kritika Sharma <35176749+KritikaSharmaKS@users.noreply.github.com>
2022-04-30 18:31:37 -0400

    update an existing article in db

--- edit-article.php
+++ edit-article.php
@@ -8,6 +8,7 @@
     if (isset($_GET['id'])) {
         $article = getArticleFromDB($connection, $_GET['id']);
         if ($article) {
+            $id = $article['id'];
             $title = $article['title'];
             $content = $article['content'];
             $published_at = $article['published_at'];
@@ -22,11 +23,28 @@
         $title = $_POST['title'];
         $content = $_POST['content'];
         $published_at = $_POST['published_at'];
-
         $errors = validateArticle($title, $content, $published_at);
-    
         if (empty($errors)) {
-            die("Form is valid");
+            $sql = "UPDATE article 
+                    SET    title = ?, 
+                           content = ?, 
+                           published_at = ? 
+                    WHERE  id = ?";
+            $stmt = mysqli_prepare($connection, $sql);
+            if ($stmt === false) {
+                echo mysqli_error($connection);
+            } else {
+                if ($published_at == '') {
+                    $published_at = null;
+                }
+                mysqli_stmt_bind_param($stmt, "sssi", $title, $content, $published_at, $id);
+                if (mysqli_stmt_execute($stmt)) {
+                    header("Location: single-article.php?id=$id");
+                    exit;
+                } else {
+                    echo mysqli_stmt_error($stmt);
+                }
+            }
         }
     }
     
