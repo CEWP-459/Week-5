c7e4676040975c5d09831150a91331c2e0c07a02
Kritika Sharma <35176749+KritikaSharmaKS@users.noreply.github.com>
2022-05-01 10:55:56 -0400

    session continuation

--- session-example.php
+++ session-example.php
@@ -2,7 +2,11 @@
 
 session_start();
 
-$_SESSION['count'] = 1;
+if (isset($_SESSION['count'])) {
+    $_SESSION['count']++;
+} else {
+    $_SESSION['count'] = 1;
+}
 
 var_dump($_SESSION['count']);
 
